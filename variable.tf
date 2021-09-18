1826e9fe62bef21583fe07ea9d0a7fa08a6388a6
Vikash Kaushik <58074696+vikash232@users.noreply.github.com>
2021-09-18 21:53:18 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+variable "subnets_cidr" {
+    type = list
+    default = ["10.0.1.0/24" , "10.0.2.0/24"]
+}
+
+variable "azs" {
+  default     = ["us-east-1b", "us-east-1e"]
+  description = "availability zones"
+}
